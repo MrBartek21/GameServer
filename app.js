990bfd5fb5a762c1090764970d3e316d8d8f647a
MrBartek21 <bartek.pacyna@onet.pl>
2020-11-04 22:59:27 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
-var http = require('http');
+const http = require('http');
 
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World!');
-}).listen(8080);
+const hostname = '127.0.0.1';
+const port = 3000;
 
-console.log("Test");
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
