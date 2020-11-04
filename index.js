2b0955c698502f09d5241e6b3b5e840756d28a9b
MrBartek21 <bartek.pacyna@onet.pl>
2020-11-04 23:21:39 +0100

    Update index.js

--- index.js
+++ index.js
@@ -10,17 +10,9 @@ express()
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 */
 
-const http = require('http');
-
-const hostname = 'localhost';
-const port = 5000;
-
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
-});
-
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+var http = require('http');
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.write('Hello World!');
+  res.end();
+}).listen(8080);
