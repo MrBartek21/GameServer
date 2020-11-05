8b568e5c216757e435b638d5734ee1d1839c1a6e
MrBartek21 <bartek.pacyna@onet.pl>
2020-11-05 01:47:40 +0100

    Update index.js

--- index.js
+++ index.js
@@ -8,11 +8,3 @@ express()
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
-
-
-/*var http = require('http');
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.write('Hello World!');
-  res.end();
-}).listen(8080);*/
